4f6ae425f25d77063bacd53e8ab80f13532b7f8f
gilleain <gilleain@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-01-08 14:24:48 +0000

    Some functions have moved or changed names, so fixing sample calls to those.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8157 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.data.sampledata/data/Javascripts/extractsmilesAndMass.js
+++ plugins/net.bioclipse.data.sampledata/data/Javascripts/extractsmilesAndMass.js
@@ -1,10 +1,10 @@
-var mols = cdk.loadMolecules("/Sample Data/SDF/Fragments2.sdf"); 
+var mols = cdk.loadMolecules("/Sample Data/SDF/Fragments2.sdf");
 
-for ( var i = 0; i < mols.size(); i++ ) { 
+for (var i = 0; i < mols.size(); i++) {
 	var mol = mols.get(i);
-	
-	print( cdk.calculateMass( mol ) +  
+
+	print(cdk.calculateMass(mol) +
 	       "\t" +
-	       cdk.calculateSmiles( mol ) +
-	       "\n" ); 
+	       cdk.calculateSMILES(mol) +
+	       "\n" );
 }
